1bca4d94ccb1f960bfaa89d634f57d05eab6f693
davegrays <daves.grays@gmail.com>
2016-03-11 18:15:20 -0800

    Update run_cmp_v2.1beta_serial.bash

--- run_cmp_v2.1beta_serial.bash
+++ run_cmp_v2.1beta_serial.bash
@@ -25,8 +25,10 @@ fi
 #edit config file to use command arguments; check if using custom parcellation or not
 if [[ $parc == NativeFreesurfer || $parc == Lausanne2008 ]];then
 	sed "s/parcellation_scheme = .*/parcellation_scheme = ${parc}/" $config | sed "s/insert_sub_here/${sub}/g" | sed "s/use_existing_freesurfer_data = .*/use_existing_freesurfer_data = ${existingFS}/" > ${sub}.ini
+	sed -i "s/insert_PWD_here/${PWD}/" ${sub}.ini
 else
 	sed 's/seg_tool = Freesurfer/seg_tool = Custom segmentation/' $config | sed 's/parcellation_scheme = Lausanne2008/parcellation_scheme = Custom/' | sed "s/insert_sub_here/${sub}/g" | sed "s/CustomParcHere/${parc}/g" | sed 's/custom_parcellation = False/custom_parcellation = True/' > ${sub}.ini
+	sed -i "s/insert_PWD_here/${PWD}/" ${sub}.ini
 	graphml=`cat ${sub}.ini | grep graphml_file | awk '{print $3}'`
 	numreg=`cat ${graphml} | grep "node id" | tail -1 | sed 's/.*="//' | sed 's/">//'`
 	echo "Using $parc custom Parcellation, with $numreg GM regions."
